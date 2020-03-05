d388d71d21f31da9051f9332ebf927c8a1836fa0
CasperCarver <pigulevsky.gleb@ya.ru>
2020-03-05 20:47:48 +0300

    feat: add solve task second case

--- src/index.js
+++ src/index.js
@@ -1,49 +1,72 @@
 module.exports = function check(str, bracketsConfig) {
-  
-  
-  let br =[];
-  for (i = 0; i<bracketsConfig.length;i++){
-    for ( j = 0; j<bracketsConfig[i].length;j++){
-        if (bracketsConfig[i][j] != "|") br.push(bracketsConfig[i][j]);
-      //br.push(bracketsConfig[i][j]);
+                    // первый вариант
+    // let str = '|()|(||)||';
+    // const config2 = [['(', ')'], ['|', '|']];
+    let br =[];
+    let pairSc='';
+    for (i = 0; i< bracketsConfig.length;i++){
+            pairSc = bracketsConfig[i][0]+bracketsConfig[i][1];
+            br.push(pairSc);
+            pairSc = "" ;
     }
-}
-// for (i=0; i<br.length;i++){
-//         if (br[i]=="|") br=br.splice(i, 2);
+    let count = 0; 
+    do{
+        count = 0;
+        for (let i = 0;i<br.length;i++){
+            
+                if (str.indexOf(br[i])!= -1){
+                str = str.replace( br[i], '');
+                count+=1;
+                } 
+        }
+    } while (count != 0);
+    return (str.length == 0) ? true : false;
+
+
+                    // второй вариант
+    //   let br =[];
+//   for (i = 0; i<bracketsConfig.length;i++){
+//     for ( j = 0; j<bracketsConfig[i].length;j++){
+//         if (bracketsConfig[i][j] != "|") br.push(bracketsConfig[i][j]);
+//       //br.push(bracketsConfig[i][j]);
+//     }
 // }
+// // for (i=0; i<br.length;i++){
+// //         if (br[i]=="|") br=br.splice(i, 2);
+// // }
   
-   br = br.join('');
-  // стек открытых скобок
-  var st = [];
+//    br = br.join('');
+//   // стек открытых скобок
+//   var st = [];
  
-  let qI = 0;
-  // бежим по всей строке
-  for (var i = 0; i < str.length; ++i) {
-      if (str[i]==" | ") qI+=1;
-      if (qI%2!=0) st.push("|")
-      // текущий символ
-      var ch = str[i];
-      // ищем символ в скобках
-      var ind = br.indexOf(ch);
-      // если скобка найдена
-      if (ind >= 0) {
-          // проверяем, какая это скобка
-          if (ind & 1) {
-              // если закрывающая скобка, проверяем стек
-              // стек пуст - плохо
-              if (!st.length) return false;
-              // извлекаем последнюю открытую скобку из стека
-              var last_br = st.pop();
-              // если она не соответствует закрывающей скобке - тоже плохо
-              if (last_br != br[ind - 1]) return false;
-          } else { 
-              // открывающую скобку просто пихаем в стек
-              st.push(ch);
-          }
-      }
-  }
-  // если после обхода всей строки стек пуст - всё ок
-  return !st.length;
+//   let qI = 0;
+//   // бежим по всей строке
+//   for (var i = 0; i < str.length; ++i) {
+//       if (str[i]==" | ") qI+=1;
+//       if (qI%2!=0) st.push("|")
+//       // текущий символ
+//       var ch = str[i];
+//       // ищем символ в скобках
+//       var ind = br.indexOf(ch);
+//       // если скобка найдена
+//       if (ind >= 0) {
+//           // проверяем, какая это скобка
+//           if (ind & 1) {
+//               // если закрывающая скобка, проверяем стек
+//               // стек пуст - плохо
+//               if (!st.length) return false;
+//               // извлекаем последнюю открытую скобку из стека
+//               var last_br = st.pop();
+//               // если она не соответствует закрывающей скобке - тоже плохо
+//               if (last_br != br[ind - 1]) return false;
+//           } else { 
+//               // открывающую скобку просто пихаем в стек
+//               st.push(ch);
+//           }
+//       }
+//   }
+//   // если после обхода всей строки стек пуст - всё ок
+//   return !st.length;
 
 }
 
@@ -82,14 +105,37 @@ function isBalanced(str) {
 
 console.log(isBalanced('||'));
 
-const config2 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
+let str = '([{}])';
+const config2 = [['(', ')'], ['[', ']'], ['{', '}']];
 let br =[];
+let pairSc='';
 for (i = 0; i<config2.length;i++){
-    for ( j = 0; j<config2[i].length;j++){
-        if (config2[i][j] != "|") br.push(config2[i][j]);
+        pairSc = config2[i][0]+config2[i][1];
+        br.push(pairSc);
+        pairSc ="";
+}
+console.log (br);
+console.log(str);
+let count = 0; 
+
+do{
+    count = 0;
+    for (let i = 0;i < br.length;i++){
+            
+            if (str.indexOf(br[i]) != -1 ){
+            str = str.replace( br[i], '');
+            
+            count+=1;
+            console.log(count + " счетчик."+ str + "строка после парсинга."+ br[i]+" поисковый элемент.");
+            } ;
     }
 
-}
+} while (count != 0);
+console.log(count);
+console.log(str + " итог");
+console.log(str.length + " длина");
+
+
 // for (i=0; i<br.length;i++){
 //     if (br[i]=="|") br=br.splice(i, 2);
 // }
